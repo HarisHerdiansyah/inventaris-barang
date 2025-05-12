87730fdc3bbd919880d7a75ff460efb926f4a314
Haris Herdiansyah <haris54237@gmail.com>
2025-05-12 18:54:19 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Inventaris Barang</title>
+</head>
+
+<body>
+  <?php
+    $message = "Hello, World!";
+  ?>
+
+  <h1>
+    <?php echo $message ?>
+  </h1>
+</body>
+
+</html>
