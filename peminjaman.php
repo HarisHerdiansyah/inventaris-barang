48e1b0097e61252fa9d6747ce25ef45745b154ca
Farell107 <farell.tirtawijaya16@gmail.com>
2025-05-19 01:06:22 +0700

    Add files via upload

---
+++ peminjaman.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+  <meta charset="UTF-8">
+  <titlE> RIWAYAT PEMINJAMAN BARANG</title>
+  <link rel="stylesheet" href="pinjam.css">
+</head>
+<body>
+  <div class="box">
+    <h1>RIWAYAT PEMINJAMAN BARANG</h1>
+    <div class="table">
+      <div class="baris header">
+        <div class="kolom">Nama Barang</div>
+        <div class="kolom">Tanggal Pinjam</div>
+        <div class="kolom">Tanggal Kembali</div>
+        <div class="kolom">Status</div>
+      </div>
+    </div>
+    <p class="empty">Belum ada data peminjaman</p>
+  </div>
+</body>
+</html>
