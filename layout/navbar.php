26249d26189c80c8c8908b2dd1deab7731c2ecc9
Haris Herdiansyah <haris54237@gmail.com>
2025-05-19 08:41:42 +0700

    fix: refactor

---
+++ layout/navbar.php
@@ -0,0 +1,4 @@
+<nav class="bg-red-500 text-white px-16 py-4 flex justify-between items-center">
+  <p class="text-2xl font-semibold">Dashboard</p>
+  <button id="logoutBtn" class="bg-slate-100 hover:bg-slate-200 cursor-pointer text-black py-2 px-4 font-semibold rounded-md">Logout</button>
+</nav>
---
+++ pages/rent-history.php
@@ -0,0 +1,34 @@
+<?php include "../layout/top.php" ?>
+<?php include "../layout/navbar.php" ?>
+
+<main class="px-16 py-4">
+  <h1 class="text-2xl font-semibold mb-4">Riwayat Peminjaman</h1>
+  <table class="border border-gray-400 min-w-full border-collapse my-10">
+    <thead>
+      <tr>
+        <th class="p-2 font-normal border border-gray-300">Timestamp</th>
+        <th class="p-2 font-normal border border-gray-300">Nama Peminjam</th>
+        <th class="p-2 font-normal border border-gray-300">Barang</th>
+        <th class="p-2 font-normal border border-gray-300">Jumlah</th>
+        <th class="p-2 font-normal border border-gray-300">Tanggal Permintaan</th>
+        <th class="p-2 font-normal border border-gray-300">Tanggal Penerimaan</th>
+        <th class="p-2 font-normal border border-gray-300">Approver</th>
+        <th class="p-2 font-normal border border-gray-300">Aksi</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td class="p-2 border border-gray-300"></td>
+        <td class="p-2 border border-gray-300"></td>
+        <td class="p-2 border border-gray-300"></td>
+        <td class="p-2 border border-gray-300"></td>
+        <td class="p-2 border border-gray-300"></td>
+        <td class="p-2 border border-gray-300"></td>
+        <td class="p-2 border border-gray-300"></td>
+        <td class="p-2 border border-gray-300"></td>
+      </tr>
+    </tbody>
+  </table>
+</main>
+
+<?php include "../layout/bottom.php" ?>
