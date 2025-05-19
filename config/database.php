ef8a08c5b1d4101ce47f98ffaad6e526a8d29159
Haris Herdiansyah <haris54237@gmail.com>
2025-05-19 10:28:45 +0700

    feat: setup database

---
+++ config/database.php
@@ -0,0 +1,15 @@
+<?php
+$env = parse_ini_file('../.env');
+$hostname = $env["DB_HOSTNAME"];
+$username = $env["DB_USER"];
+$password = $env["DB_PASSWORD"];
+$db_name = $env["MODE"] === "development" ? $env["DB_NAME_DEV"] : $env["DB_NAME_PROD"];
+$port = $env["DB_PORT"];
+
+$db = new mysqli($hostname, $username, $password, $db_name, $port);
+
+if ($db->connect_error) {
+  die("Connection Failed");
+}
+
+echo "Connection Success";
